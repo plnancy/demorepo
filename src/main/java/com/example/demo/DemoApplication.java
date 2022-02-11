968cf26e64cb6389e323c60a28e8475582ce21a2
plnancy <nancy.neelamma@gmail.com>
2022-02-11 13:00:43 +0530

    My first intellij commit.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//Main Method
+//This is Main Method
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
